b266f7ee79a9be71896bd2d127a89d470eb5723c
Ben Spitz <1844060+diracdeltafunk@users.noreply.github.com>
2025-07-09 20:37:34 -0400

    some work on cong_lattice with new realization

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hccr
+# hccr [![Rust](https://github.com/diracdeltafunk/hccr/actions/workflows/rust.yml/badge.svg)](https://github.com/diracdeltafunk/hccr/actions/workflows/rust.yml)
 
     Homotopical    
     Combinatorics  
--- src/cong_lattice.rs
+++ src/cong_lattice.rs
@@ -1,6 +1,34 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
 
+// Indexes into a lower-triangular matrix
+// e.g. for n = 4 we have
+// 0 1 2 3
+// 4 5 6 _
+// 7 8 _ _
+// 9 _ _ _
+// so 2 -> (0,2) and 7 -> (2,0).
+fn idx_to_pair(n: usize, idx: usize) -> (usize, usize) {
+    assert!(
+        n + 1 < (usize::MAX as f64).sqrt() as usize && 2 * idx < n * (n + 1),
+        "Out of bounds!"
+    );
+    let r = (0.5 + (n as f64) - ((0.5 + (n as f64)).powi(2) - 2.0 * (idx as f64)).sqrt()) as usize;
+    (r, idx - (1 + 2 * n - r) * r / 2)
+}
+
+// Gets index into a lower-triangular matrix from a pair (r, c)
+// e.g. for n = 4 we have
+// 0 1 2 3
+// 4 5 6 _
+// 7 8 _ _
+// 9 _ _ _
+// so (0,2) -> 2 and (2,1) -> 8.
+fn pair_to_idx(n: usize, (r, c): (usize, usize)) -> usize {
+    assert!(r < n && c < n, "Indices out of bounds.");
+    (1 + 2 * n - r) * r / 2 + c
+}
+
 // A CongLattice encodes the data of a finite lattice, together with an
 // equivalence relation on its elements which is a congruence relation on the
 // underlying poset in the sense of universal algebra. The data structure also
@@ -8,6 +36,10 @@ use std::fmt::Debug;
 pub struct CongLattice {
     pub num_elements: usize,
     poset_relation: Vec<Vec<bool>>,
+    // TODO Actually, we should be storing an equivalence relation on edges...
+    //      Not sure exactly what axioms this equivalence relation needs to
+    //      satisfy. One condition must be that the induced equivalence relation
+    //      on the elements (i.e. identity edges) is a poset congruence relation
     equivalence_relation: Vec<Vec<bool>>,
     meet_cache: HashMap<(usize, usize), usize>,
     join_cache: HashMap<(usize, usize), usize>,
@@ -151,6 +183,29 @@ impl CongLattice {
             .insert((a, b), corestriction.clone());
         corestriction
     }
+    // Returns the equivalence class of the input element
+    pub fn equivalence_class(&self, a: usize) -> Vec<usize> {
+        assert!(a < self.num_elements, "Index out of bounds.");
+        let mut class = Vec::new();
+        for i in 0..self.num_elements {
+            if self.equivalence_relation[a][i] {
+                class.push(i);
+            }
+        }
+        class
+    }
+    // Takes a subset of elements and returns the union of their equivalence classes
+    pub fn equivalence_closure(&self, elements: &[usize]) -> Vec<usize> {
+        let mut closure = Vec::new();
+        for &elem in elements {
+            for i in self.equivalence_class(elem) {
+                closure.push(i);
+            }
+        }
+        closure.sort_unstable();
+        closure.dedup();
+        closure
+    }
 }
 
 impl Debug for CongLattice {
@@ -163,6 +218,34 @@ impl Debug for CongLattice {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_idx_to_pair() {
+        assert_eq!(idx_to_pair(4, 0), (0, 0));
+        assert_eq!(idx_to_pair(4, 1), (0, 1));
+        assert_eq!(idx_to_pair(4, 2), (0, 2));
+        assert_eq!(idx_to_pair(4, 3), (0, 3));
+        assert_eq!(idx_to_pair(4, 4), (1, 0));
+        assert_eq!(idx_to_pair(4, 5), (1, 1));
+        assert_eq!(idx_to_pair(4, 6), (1, 2));
+        assert_eq!(idx_to_pair(4, 7), (2, 0));
+        assert_eq!(idx_to_pair(4, 8), (2, 1));
+        assert_eq!(idx_to_pair(4, 9), (3, 0));
+    }
+
+    #[test]
+    fn test_pair_to_idx() {
+        assert_eq!(pair_to_idx(4, (0, 0)), 0);
+        assert_eq!(pair_to_idx(4, (0, 1)), 1);
+        assert_eq!(pair_to_idx(4, (0, 2)), 2);
+        assert_eq!(pair_to_idx(4, (0, 3)), 3);
+        assert_eq!(pair_to_idx(4, (1, 0)), 4);
+        assert_eq!(pair_to_idx(4, (1, 1)), 5);
+        assert_eq!(pair_to_idx(4, (1, 2)), 6);
+        assert_eq!(pair_to_idx(4, (2, 0)), 7);
+        assert_eq!(pair_to_idx(4, (2, 1)), 8);
+        assert_eq!(pair_to_idx(4, (3, 0)), 9);
+    }
 
     #[test]
     fn test_meet() {
