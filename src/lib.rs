b4a61add10458ee2212a8e1d3278d8f2aa552c46
Ben Spitz <1844060+diracdeltafunk@users.noreply.github.com>
2025-09-04 14:02:36 -0400

    Change library name to `hccr`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-//! # hccrlib
-//!
-//! `hccrlib` is a library to make calculations in homotopical combinatorics easier.
+//! `hccr` is a library to make calculations in homotopical combinatorics easier.
 
 pub mod cong_lattice;
 pub mod fca;
